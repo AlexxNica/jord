bc91a86680ece1f9e224f03d10bba4db9ee29318
Rene Hangstrup Møller <rhmoller@gmail.com>
2017-02-14 21:47:01 +0100

    build(linting): lint source with TSLint

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.ts",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "jord.js",
-    library: "jord",
-    libraryTarget: "umd"
-  },
-  module: {
-    rules: [
-      { test: /\.ts$/, loader: "ts-loader", exclude: /node_modules/ }
-    ]
-  },
-  resolve: {
-    extensions: [".ts", ".js"]
-  }
+    entry: "./src/index.ts",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "jord.js",
+        library: "jord",
+        libraryTarget: "umd"
+    },
+    module: {
+        rules: [
+            {test: /\.ts$/, enforce: "pre", loader: "tslint-loader"},
+            {test: /\.ts$/, loader: "ts-loader", exclude: /node_modules/}
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    }
 };
